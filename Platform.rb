5763e815c6709e54b4c11c1ab63118dd5ed40a78
PiperS52 <simon.a.piper87@gmail.com>
2021-05-29 14:13:46 +0100

    updated and removed errors

--- Platform.rb
+++ Platform.rb
@@ -8,6 +8,7 @@
 #4. mean LTV of all applications
 
 class Platform 
+  
   def initialize 
     @applications = []
     @written_loans = []
@@ -28,24 +29,25 @@ class Platform
 
   def avg_ltv_applications
     #returns mean ltv of all apps
-    mean = @total_ltvs.sum() / @total.ltvs.length()
+    mean = (@total_ltvs.sum().to_f / @total_ltvs.length()).round(1)
     puts "The mean average LTV of all applications to date is #{mean}"
   end 
 
   def make_application(loan_amount, asset_value, credit_score)
-    ltv = (loan_amount/asset_value)*100
+    #creates new application, giving it the appropriate stataus
+    ltv = ((loan_amount.to_f/asset_value)*100).round(0)
     @total_ltvs.push(ltv)
     if loan_amount > 1500000 || loan_amount < 100000
       @applications.push("declined")
-    elsif loan_amount > 1000000 && ltv <= 60 && credit_score => 950
+    elsif (loan_amount >= 1000000) && (ltv <= 60) && (credit_score >= 950)
       @applications.push("successful")
       @written_loans.push(loan_amount)
     elsif loan_amount < 1000000
-      if (ltv < 60 && credit_score => 750) || (ltv < 80 && credit_score => 800) || (ltv < 90 && credit_score => 900)
-        @application.push("successful")
+      if (ltv < 60 && credit_score >= 750) || (ltv < 80 && credit_score >= 800) || (ltv < 90 && credit_score >= 900)
+        @applications.push("successful")
         @written_loans.push(loan_amount)
-      elsif (ltv > 90)
-        @application.push("declined")
+      else  
+        @applications.push("declined")
       end  
     else
       @applications.push("declined")
@@ -54,4 +56,6 @@ class Platform
 
 end 
 
-# future - application class, create method
+# future - extract out an Application class (with create/write method), and modify 
+# Platform to ie. Reporting class
+# testing
